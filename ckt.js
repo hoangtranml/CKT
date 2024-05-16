6f99713c0278b8edb2a4e6fc854ab8485da1c2e8
hoangtranml <162419986+hoangtranml@users.noreply.github.com>
2024-05-16 20:04:21 +0700

    Add files via upload

---
+++ ckt.js
@@ -0,0 +1,22 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+const list = document.querySelectorAll('.list');
+function activeLink() {
+    list.forEach((item) =>
+    item.classList.remove('active'));
+    this.classList.add('active');
+}
+list.forEach((item) =>
+item.addEventListener('click',activeLink));
---
+++ information.js
@@ -0,0 +1,18 @@
+
+const parentContainer =  document.querySelector('.list-center');
+
+parentContainer.addEventListener('click', event=>{
+
+    const current = event.target;
+
+    const isReadMoreBtn = current.className.includes('read-more-btn');
+
+    if(!isReadMoreBtn) return;
+
+    const currentText = event.target.parentNode.querySelector('.read-more-text');
+
+    currentText.classList.toggle('read-more-text--show');
+
+    current.textContent = current.textContent.includes('Xem thêm') ? "Ẩn bớt..." : "Xem thêm...";
+
+})
